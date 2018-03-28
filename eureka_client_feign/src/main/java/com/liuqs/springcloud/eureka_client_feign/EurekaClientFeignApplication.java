5d25c672d6497ee38b0ab7d8a437f75d89d31460
liuqianshun <qianshunliu_1994@163.com>
2018-03-28 11:58:33 +0800

    :sweat_drops:Feign基本使用

---
+++ eureka_client_feign/src/main/java/com/liuqs/springcloud/eureka_client_feign/EurekaClientFeignApplication.java
@@ -0,0 +1,16 @@
+package com.liuqs.springcloud.eureka_client_feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class EurekaClientFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientFeignApplication.class, args);
+	}
+}
